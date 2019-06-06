c672cca08cb31b66e4a625443ff56cdcf7b4e1d2
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-06 00:50:02 -0300

    aula 37 - trabalhando com eventos

---
+++ src/components/Membro/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Membro extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      nome: props.nome
+    };
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar() {
+    this.setState({nome: 'Matheus'});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Olá {this.state.nome}</h1>
+        <button onClick={this.entrar}>Entrar</button>
+        <button onClick={() => this.setState({nome: 'Visitante'})}>Sair</button>
+      </div>
+    );
+  }
+
+}
+
+
+export default Membro;
