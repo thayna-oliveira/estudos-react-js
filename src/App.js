0fed204d0096aea4143d42141c6efa9648fafe16
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-06 21:55:34 -0300

    Aula 38 - Formulario de login basico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
-
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: true
+    };
+
+    this.logoff = this.logoff.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  logoff(){
+    this.setState({status: false});
+  }
+
+  login(){
+    this.setState({status: true});
+  }
+
   render() {
     return (
       <div>
-        <Membro nome="Visitante"/>
+        {
+          this.state.status ?
+            <div>
+              <h1>Bem-vindo ao sistema</h1>
+              <button onClick={this.logoff}>Fazer logoff</button>
+            </div> :
+            <div>
+              <h1>Olá visitante, faça o login!</h1>
+              <button onClick={this.login}>Fazer login</button>
+            </div>
+        }
       </div>
     );
   }
