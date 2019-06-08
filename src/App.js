033dfec75815501c7af8fedc1d6a9330e6b6e460
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-07 22:16:02 -0300

    Aula 40 - Trabalhando com formularios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
 import React, { Component } from 'react';
-import Feed from './components/Feed'
+import { throwStatement } from '@babel/types';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      feed: [
-        {id: 1, username: 'Matheus', curtidas: 30, comentarios: 35},
-        {id: 2, username: 'Lucas', curtidas: 45, comentarios: 0}
-      ]
+      email: '',
+      senha: '',
+      sexo: ''
     };
 
+    this.trocaEmail = this.trocaEmail.bind(this);
+    this.trocaSenha = this.trocaSenha.bind(this);
+    this.trocaSexo = this.trocaSexo.bind(this);
+
+  }
+
+  trocaEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  trocaSenha(e) {
+    this.setState({ senha: e.target.value });
+  }
+
+  trocaSexo(e) {
+    this.setState({ sexo: e.target.value });
   }
 
   render() {
     return (
       <div>
-      {this.state.feed.map((item) => {
-        return (
-          <Feed id={item.id} username={item.username}
-          curtidas={item.curtidas} comentarios={item.comentarios}/>
-        );
-         })}
+        E-mail:
+        <input type="email" name="email" value={this.state.email}
+          onChange={this.trocaEmail}></input>
+        <br></br>
+        Senha:
+        <input type="password" name="senha" value={this.state.senha}
+          onChange={this.trocaSenha}></input>
+        <br />
+        Sexo:
+        <select name="sexo" value={this.state.sexo} onChange={this.trocaSexo}>
+          <option value="">Selecione...</option>
+          <option value="M">Masculino</option>
+          <option value="F">Feminino</option>
+        </select>
+
+        <h2>{this.state.email}</h2>
+        <h2>{this.state.senha}</h2>
+        <h2>{this.state.sexo}</h2>
       </div>
     );
   }
