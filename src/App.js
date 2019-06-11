a84d6802541d73c39ff7f7ad6da2a6433546b97e
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-11 09:05:52 -0300

    Aula 41 - Praticando formularios

--- src/App.js
+++ src/App.js
@@ -7,27 +7,54 @@ class App extends Component {
     super(props);
 
     this.state = {
-      feed: [
-        {id: 1, username: 'Matheus', curtidas: 30, comentarios: 35},
-        {id: 2, username: 'Lucas', curtidas: 45, comentarios: 0}
-      ]
+      nome: '',
+      email: '',
+      senha: '',
+      error: ''
     };
+
+    this.cadastrar = this.cadastrar.bind(this);
+
+  }
+
+  cadastrar(event){
+
+    const {nome,email,senha} = this.state;
+
+    if(nome !== '' && email !== '' && senha !== '' ) {
+      alert(`Nome: ${nome} \nEmail: ${email}\nSenha: ${senha}`);
+    } else {
+      this.setState({error:'Ops! Está faltando algo!'})
+    }
+    
+
+    event.preventDefault();
 
   }
 
   render() {
     return (
       <div>
-      {this.state.feed.map((item) => {
-        return (
-          <Feed id={item.id} username={item.username}
-          curtidas={item.curtidas} comentarios={item.comentarios}/>
-        );
-         })}
+        <h1>Novo usuário </h1>
+        {this.state.error  && <p>{this.state.error}</p>}
+        <form onSubmit={this.cadastrar}>
+          <label>Nome:</label>
+          <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/>
+          <label>E-mail:</label>
+          <input type="email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
+          <label>Senha:</label>
+          <input type="password" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}/>
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        {this.state.nome}
+
+        {this.state.email}
+
+        {this.state.senha}
       </div>
     );
   }
 }
-
 
 export default App;
