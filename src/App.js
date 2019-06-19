2af1a19c8001a1b97b63bfa4c747f080a3570b61
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-19 09:05:16 -0300

    Aula 45 - Requisições HTTP

--- src/App.js
+++ src/App.js
@@ -7,54 +7,38 @@ class App extends Component {
     super(props);
 
     this.state = {
-      numero: 0,
-      botao: "Iniciar"
-    }
-    this.timer = null;
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
-
+      nutri: []
+    };
   }
 
-  vai() {
-    let state = this.state;
+  componentDidMount(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
 
-    if (this.timer !== null) {
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = "Iniciar";
-     
-    } else {
-      state.botao = "Pausar"; 
-      this.timer = setInterval(() => {
-        let state = this.state; 
-        state.numero += 0.1;
-        this.setState(state);
-      }, 100);
-    }
-
-    this.setState(state);
-  }
-
-  limpar() {
-
-    let state = this.state;
-    clearInterval(this.timer);
-    this.timer = null;
-    state.numero = 0;
-    state.botao = "Iniciar";
-    this.setState(state);
+    fetch(url)
+    .then((r) => r.json())
+    .then((json)=> {
+      let state = this.state;
+      state.nutri = json;
+      this.setState(state);
+      console.log(json);
+    });
   }
 
   render() {
     return (
       <div className="container">
-        <img className="img" src={require('./assets/cronometro.png')} />
-        <a className="timer">{this.state.numero.toFixed(1)}</a>
-        <div className="areaBtn">
-          <a className="botao" onClick={this.vai}>{this.state.botao}</a>
-          <a classname="botao" onClick={this.limpar}>Limpar</a>
-        </div>
+        {this.state.nutri.map((item) => {
+            return(
+              <article key={item.id} classaName="post">
+                <strong className="titulo">{item.titulo}</strong>
+                <img className="capa" src={item.capa}></img>
+                <p className="subtitulo">{item.subtitulo}</p>
+                <a className="botao" href="#">Acessar</a>
+                
+              </article>
+
+            );
+        })}
       </div>
     );
   }
