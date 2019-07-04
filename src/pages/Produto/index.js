412ca5cdb3dc5e40cf95f007cad3a5de8fed81a3
thayna-oliveira <oliveira.thayna@outlook.com>
2019-07-04 09:18:35 -0300

    Aula 48 - Melhorando Rotas

---
+++ src/pages/Produto/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'; 
+
+class Produto extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: ''
+    };
+  }
+
+  componentDidMount(){
+    const{id} = this.props.match.params;
+    this.setState({id: id});
+  }
+
+  render() {
+    return (
+      <div>
+
+        <section>
+           <h2>Produto {this.state.id}</h2> 
+        </section>
+
+      </div>
+    );
+  }
+}
+
+export default Produto;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
 import Erro from './pages/Erro';
+import Produto from './pages/Produto';
+
 
 const Routes = () => {
     return (
@@ -11,7 +13,8 @@ const Routes = () => {
             <Header/>
             <Switch>
                 <Route exact path="/" component={Home}/>
-                <Route exact path="/about" component={About}/>
+                <Route exact path="/about" component={About}/> 
+                <Route path="/produto/:id" component={Produto}/>
                 <Route path="*" component={Erro}/>
             </Switch>
         </BrowserRouter>
