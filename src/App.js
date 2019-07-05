8d14bd83fd7b79949e231b13a672f66b261e96ad
thayna-oliveira <oliveira.thayna@outlook.com>
2019-07-05 09:03:56 -0300

    Aula 49 - Projeto filmes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import Routes from './routes';
+import Routes from './routes'; 
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <Routes/>
       </div>
     );
