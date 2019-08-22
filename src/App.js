3ed2bf162ffbef940a7a1dbbe779c7c535578152
thayna-oliveira <oliveira.thayna@outlook.com>
2019-08-22 17:22:00 -0300

    Aula 57 - Adicionando o firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'; 
-import Routes from './routes';
+import React, {Component} from 'react'; 
+import firebase from 'firebase';
 
-function App() {
+export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {};
+
+    // Your web app's Firebase configuration
+    let firebaseConfig = {
+      apiKey: "AIzaSyDC4g-M_OsoPhi5GRjVUsu9SsTq9gGNSXA",
+      authDomain: "reactapp-9eb60.firebaseapp.com",
+      databaseURL: "https://reactapp-9eb60.firebaseio.com",
+      projectId: "reactapp-9eb60",
+      storageBucket: "",
+      messagingSenderId: "935896278445",
+      appId: "1:935896278445:web:b6d8cc16654e82c7"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render(){
     return (
-      <Routes></Routes>
+      <div>Funcionando!</div>
     );
+  }
 }
-
-export default App;
